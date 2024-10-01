2a89e2adc58ede0ae279aafdbddcbd7457118504
tejaby <yostintejaxun20@gmail.com>
2024-10-01 17:26:12 -0600

    Include user information in login response

--- user/api/api.py
+++ user/api/api.py
@@ -1,11 +1,15 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from django.contrib.auth import authenticate
+
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
+
+from .serializers import UserSerializer
 
 
 '''
@@ -16,9 +20,18 @@ Vista basada en clase TokenObtainPairView para la autenticacion de usuarios y cr
 
 class TokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
+
+        user = authenticate(request=request, username=request.data.get(
+            'username'), password=request.data.get('password'))
+
+        if user is None:
+            return Response({'error': 'no se encontró ninguna cuenta activa con las credenciales proporcionadas'}, status=HTTP_400_BAD_REQUEST)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response({'tokens': serializer.validated_data, 'message': 'inicio de sesion exitoso'})
+        user_serializer = UserSerializer(user)
+
+        return Response({'token': serializer.validated_data, 'user': user_serializer.data, 'message': 'inicio de sesion exitoso'})
 
 
 '''
